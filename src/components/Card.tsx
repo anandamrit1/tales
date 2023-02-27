bf24728e336365f307a3b44be89398588002162b
anandamrit1 <anandamrit53@gmail.com>
2023-02-28 00:10:27 +0530

    css fixes & .find profile on sidenav

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,36 +1,36 @@
 import { Avatar, Tag } from 'degen';
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import { Article, Author } from 'types/types';
+import { ArticleType, Author } from 'types/types';
 
 export type ArticleCardProps = {
-    article: Article;
-    author: Author;
+    article: ArticleType;
 }
 
-const ArticleCard = ({article, author} : ArticleCardProps) => {
+const ArticleCard = ({article} : ArticleCardProps) => {
   const navigate = useNavigate()
 
   return (
     <div
-      onClick={() => navigate(`/${author.address}/${article.id}`)}
-     className="border border-gray-200 rounded-xl max-w-xl overflow-hidden cursor-pointer hover:border-gray-300">
+      onClick={() => navigate(`/${article.authorAddress}/${article.id}`)}
+     className="border border-gray-200 rounded-xl max-w-xl overflow-hidden h-96 flex flex-col justify-between cursor-pointer hover:border-gray-300">
+      <div className="">
       <img
         className="object-cover w-full h-48"
         src={article.coverImg}
         alt={article.title}
       />
-      <div className="p-6">
-        <Tag size='small'>
+      
+        <p className='text-sm p-4'>
           {new Date(article.createdAt).toLocaleDateString()}
-        </Tag>
-        <h2 className="text-base font-medium text-gray-800 my-2">{article.title}</h2>
-        <div className="flex items-center">
+        </p>
+        <h2 className="text-base font-medium text-gray-800 px-4">{article.title}</h2>
+      </div>
+      <div className="flex items-center p-4">
           <Avatar label={article.authorName} size="6" src={article.authorImg} />
           <p className='px-2 font-medium text-sm'>{article.authorName}</p>
-          <Tag size='small'>{article.authorAddress}</Tag>
+          <p onClick={() => navigate(`/${article.authorAddress}`)} className='bg-gray-100 text-xs max-w-min py-1 px-2 rounded-full hover:bg-gray-200'>{article.authorAddress}</p>
         </div>
-      </div>
     </div>
   );
 };
--- src/components/SideNav.tsx
+++ src/components/SideNav.tsx
@@ -1,6 +1,8 @@
 // @ts-ignore
 import * as fcl from '@onflow/fcl'
 import classNames from "classnames";
+import { useAuthor } from 'hooks/useAuthor';
+import useCurrentUser from 'hooks/useCurrentUser';
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Logo } from "../images"
@@ -25,6 +27,9 @@ const NavData: NavBarGroupData = [
     ]
 
 function SideNav({ selectedTab }: { selectedTab: TabType }) {
+  const user = useCurrentUser()
+  const { author } = useAuthor(user?.addr)
+
   return (
     <>
       <div className="overflow-y-auto py-4 px-3 bg-white-100 h-full w-[300px] flex flex-col justify-between">
@@ -43,18 +48,21 @@ function SideNav({ selectedTab }: { selectedTab: TabType }) {
             </div>
         </div>
         <div className="flex pl-4">
-            <img 
-                src={"https://d2qp0siotla746.cloudfront.net/img/use-cases/profile-picture/template_0.jpg"}
+            {
+              author?.img && 
+              <img 
+                src={author?.img}
                 className="rounded-full h-10"
                 alt="Flowbite Logo"
-            />
+              />
+            }
             <div className="flex flex-col ml-2 w-52">
-                <div className="text-sm font-semibold">John Doe</div>
+                {author?.name && <div className="text-sm font-semibold">{author?.name}</div>}
                 <div className="text-xs text-gray-500">
-                    0x000319313
+                    {author?.findName != "" ? author?.findName : user?.addr}
                 </div>
             </div>
-            <span onClick={() => fcl.unauthenticate()} className="material-icons text-sm self-center cursor-pointer">logout</span>
+            <span onClick={() => fcl.unauthenticate()} className="material-icons text-base px-2 pt-1 w-8 h-8 hover:bg-gray-100 rounded-full self-center cursor-pointer">logout</span>
         </div>
       </div>
     </>
