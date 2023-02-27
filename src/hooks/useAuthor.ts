07fcbb8b378b99e66fbc56b6bb221211fa439df6
anandamrit1 <anandamrit53@gmail.com>
2023-02-27 23:06:06 +0530

    useAuthor hook to get find profile

--- src/hooks/useAuthor.ts
+++ src/hooks/useAuthor.ts
@@ -1,36 +1,65 @@
-import { useEffect, useMemo, useState } from "react";
+import { useCallback, useEffect, useMemo, useState } from "react";
 // @ts-ignore
 import * as fcl from '@onflow/fcl'
 // @ts-ignore
 import GetFindProfile from '../cadence/scripts/GetFindProfile.cdc'
+// @ts-ignore
+import GetAddressFromFindName from '../cadence/scripts/GetAddressFromFindName.cdc'
 import { defaultAuthor } from "utils/constants";
 import { Author } from "types/types";
 
-export const useAuthor = (address: string): Author => {
-    const [currAuthor, setCurrAuthor] = useState<any>()
+export type useAuthorType = {
+    author: Author
+    isAuthorLoading: boolean
+}
 
-    useEffect(() => {
-        const getProfile = async () => {
-            const tempAuthor = defaultAuthor
-            var res;
-            try {
+export const useAuthor = (address: string): useAuthorType => {
+    const [currAuthor, setCurrAuthor] = useState<any>()
+    const [isAuthorLoading, setIsAuthorLoading] = useState<boolean>(true)
+
+    const getProfile = useCallback(async () => {
+        const tempAuthor = defaultAuthor
+        setIsAuthorLoading(true)
+        var res, addressToSearch = address;
+        try {
+            if (address.endsWith(".find")) {
+                addressToSearch = address.replace(".find", "")
                 res = await fcl.query({
-                    cadence: GetFindProfile,
-                    args: (arg: any, t: any):any => [arg(address, t.Address)]
+                    cadence: GetAddressFromFindName,
+                    args: (arg: any, t: any):any => [arg(addressToSearch, t.String)]
                 })
-                console.log("Profile : ", res)
-            } catch(e) {
-                console.log("Profile Error: ", e)
+                addressToSearch = res
+                console.log("Address : ", res)
             }
-            tempAuthor.name = res?.name
-            tempAuthor.findName = res?.findName ?? tempAuthor.findName
-            tempAuthor.description = res?.description ?? tempAuthor.description
-            tempAuthor.img = res?.avatar
-            setCurrAuthor(tempAuthor)
+            if (addressToSearch === "") {
+                setCurrAuthor(tempAuthor)
+                setIsAuthorLoading(false)
+                return
+            }
+
+            res = await fcl.query({
+                cadence: GetFindProfile,
+                args: (arg: any, t: any):any => [arg(addressToSearch, t.Address)]
+            })
+            console.log("Profile : ", res)
+        } catch(e) {
+            console.log("Profile Error: ", e)
+        } finally {
+            setIsAuthorLoading(false)
         }
-    
-        getProfile()
+
+        tempAuthor.name = res?.name
+        tempAuthor.findName = res?.findName ?? tempAuthor.findName
+        tempAuthor.description = res?.description ?? tempAuthor.description
+        tempAuthor.img = res?.avatar
+        tempAuthor.address = res?.address
+        
+        setCurrAuthor(tempAuthor)
     }, [address])
 
-    return currAuthor
+    useEffect(() => {
+        if (address) getProfile()
+    }, [address])
+
+    return { author: currAuthor, isAuthorLoading}
 }
