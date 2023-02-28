4799beccfd4c63afe147e855da7c03b7bd1a167f
anandamrit1 <anandamrit53@gmail.com>
2023-03-01 02:46:41 +0530

    fixes

--- src/components/PaymentModel.tsx
+++ src/components/PaymentModel.tsx
@@ -16,7 +16,7 @@ function PaymentModel({priceInitial, onSubscribe} : PaymentModelProps) {
     <div>
         <div className="flex flex-col items-center justify-center z-[10000]">
             <div className="flex flex-col bg-white-100 rounded-lg w-96 p-6">
-                <p className="text-xl font-black ">Enter <span className="text-green-900">Price</span></p>
+                <p className="text-xl font-black ">Enter Price in <span className="text-green-900">Flow</span></p>
                 <input onChange={(e) => setPrice(Number(e.target.value))} value={price} className="px-4 py-3 rounded-lg outline-none border-[1px] border-gray-300 mt-4" type="number" step="0.1" placeholder="0.0" />
                 <button onClick={handleSubscribe} className="bg-green-900 hover:bg-green-800 text-white font-bold py-2 px-6 rounded-lg cursor-pointer mt-4">Set Price</button>
             </div> 
--- src/pages/Subscribers.tsx
+++ src/pages/Subscribers.tsx
@@ -1,9 +1,35 @@
 import { BodyLayout } from 'components/BodyLayout'
 import SideNav from 'components/SideNav'
-import React from 'react'
+import useCurrentUser from 'hooks/useCurrentUser'
+import React, { useEffect } from 'react'
+import { getSubscribersEndpoint } from 'utils/constants'
 
 
 function Subscribers() {
+  const [subsribers, setSubscribers] = React.useState<any[]>()
+  const user = useCurrentUser()
+  
+  useEffect(() => {
+    const getSubscribers = async (address: string) => {
+      try {
+          const res = await fetch(`${getSubscribersEndpoint}?authorAddress=${address}`, {
+              method: 'GET',
+              headers: {
+                  'Content-Type': 'application/json'
+              }
+          })
+          const data = await res.json()
+          console.log(data)
+      } catch(e) {
+          console.log(e)
+      } 
+    }
+
+    if (user && user?.addr) {
+        getSubscribers(user?.addr)
+    }
+  }, [user])
+
   return (
     <BodyLayout>
         <SideNav selectedTab='Subscribers' />
