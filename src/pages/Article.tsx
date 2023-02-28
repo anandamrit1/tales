d65b2e47d451c688a2f6227f5ff2340c0e7cf400
anandamrit1 <anandamrit53@gmail.com>
2023-03-01 02:47:17 +0530

    entire flow for article fixed

--- src/pages/Article.tsx
+++ src/pages/Article.tsx
@@ -9,7 +9,19 @@ import { useAuthor } from "hooks/useAuthor";
 import edjsParser from 'editorjs-parser'
 import SubscribeModel from "components/SubscribeModel";
 import { FlowLogo } from "images";
+// @ts-ignore
+import * as fcl from '@onflow/fcl'
+// @ts-ignore
+import GetPartialPostById from '../cadence/scripts/GetPartialPostById.cdc'
+// @ts-ignore
+import GetArticle from '../cadence/scripts/GetArticle.cdc'
+// @ts-ignore
+import PurchaseArticle from '../cadence/transactions/PurchaseArticle.cdc'
 
+import useCurrentUser from "hooks/useCurrentUser";
+import { subscribeTxStatus } from "utils/subscribeTxStatus";
+import Loader from "components/Loader";
+import { SkeletonLoader } from "components/Skeleton";
 
 type PostProps = {
     post?: ArticleType;
@@ -24,13 +36,40 @@ const Article = ({ post, isPreview = false, authorIdForPreview }: PostProps) => 
     const { author, isAuthorLoading } = useAuthor(isPreview ? authorIdForPreview : authorId)
     const parser = new edjsParser();
     const navigate = useNavigate()
-
+    const user = useCurrentUser();
 
     const [loading, setLoading] = useState(false);
     const [tipAmount, setTipAmount] = useState(0);
     const [article, setArticle] = useState<ArticleType>();
     const [show, setShow] = useState(false);
+    const [showArticle, setShowArticle] = useState(false);
+    const [showLogin, setShowLogin] = useState(false);
 
+
+    const purchaseArticle = async (authorAddress: string, postId: String, price: number) => {
+        try {
+            const transactionId = await fcl.mutate({
+                cadence: PurchaseArticle,
+                args: (arg: any, t: any) => [arg(authorAddress, t.Address), 
+                                   arg(postId, t.UInt64), 
+                                   arg(price.toFixed(2), t.UFix64)]
+              })
+
+              subscribeTxStatus(transactionId)
+        } catch (e) {
+            console.error(e)
+        } finally {
+            setLoading(false)
+        }
+    }
+
+    const handlePaidArticle = () => {
+        if (showLogin) {
+            fcl?.authenticate()
+        } else if (article?.price > 0 && article?.content?.blocks?.length == 0) {
+            purchaseArticle(authorId, article?.id, article?.price)
+        } 
+    }
 
     useEffect(() => {
         if (isPreview) {
@@ -40,28 +79,134 @@ const Article = ({ post, isPreview = false, authorIdForPreview }: PostProps) => 
 
 
     useEffect(() => {
-        const getArticlesByAuthor = async (address: string) => {
-            const article = demoPost
-            // await fetch(`http://localhost:3000/api/articles?author=${address}`)
-            //     .then(res => res.json())
-            setArticle(article)
+        const getMyArticleById = async (address: string, postId: String) => {
+            let res;
+            try {
+              res = await fcl.query({
+                cadence: GetPartialPostById,
+                args: (arg: any, t: any) => [arg(address, t.Address), arg(postId, t.UInt64)]
+              })
+            } catch(e) { res = []; console.log(e) }
+            console.log("Res: ", res)
+            return res
         }
-        if (author && author.address && !isPreview) {
+
+        const getPaidArticle = async (authorAddress: string, readerAddress: string, postId: String) => {
+            let res;
+            try {
+              res = await fcl.query({
+                cadence: GetArticle,
+                args: (arg: any, t: any) => [arg(authorAddress, t.Address), arg(readerAddress, t.Address), arg(postId, t.UInt64)]
+              })
+            } catch(e) { res = []; console.log(e) }
+            console.log("Res: ", res)
+            return res
+        }
+
+        const getArticlesByAuthor = async (address: string) => {
+            const articleById = await getMyArticleById(address, articleId)
+
+            if (parseInt(articleById.price) === 0) {
+                const data : any = await fetch(articleById.data).then(res => res.json())
+                const article: ArticleType = {
+                    authorAddress: articleById.author,
+                    authorName: "",
+                    authorDesc: "",
+                    authorImg: "",
+                    title: articleById.title,
+                    content: data,
+                    coverImg: articleById.image,
+                    readTime: 0,
+                    createdAt: new Date(parseInt(articleById.createDate) * 1000).toDateString(),
+                    id: articleById.id,
+                    likes: 0,
+                    price: parseInt(articleById.price),
+                }
+                setShowArticle(true)
+                setArticle(article)
+            } else {
+                if (!user?.addr) {
+                    setShowLogin(true)
+                    const article: ArticleType = {
+                        authorAddress: articleById.author,
+                        authorName: "",
+                        authorDesc: "",
+                        authorImg: "",
+                        title: articleById.title,
+                        content: {blocks: []},
+                        coverImg: articleById.image,
+                        readTime: 0,
+                        createdAt: new Date(parseInt(articleById.createDate) * 1000).toDateString(),
+                        id: articleById.id,
+                        likes: 0,
+                        price: parseInt(articleById.price),
+                    }
+                    
+                    setArticle(article)
+                    setShowArticle(false)
+                } else {
+                    const paidArticle = await getPaidArticle(address, user?.addr, articleId)
+                    let data: any = {blocks: []}
+                    setShowLogin(false)
+
+                    if (paidArticle) {
+                        data = await fetch(paidArticle.data.replace("ipfs.io", "nftstorage.link")).then(res => res.json())
+                        const article: ArticleType = {
+                            authorAddress: paidArticle.author,
+                            authorName: "",
+                            authorDesc: "",
+                            authorImg: "",
+                            title: paidArticle.title,
+                            content: data,
+                            coverImg: paidArticle.image,
+                            readTime: 0,
+                            createdAt: new Date(parseInt(paidArticle.createDate) * 1000).toDateString(),
+                            id: paidArticle.id,
+                            likes: 0,
+                            price: parseInt(paidArticle.price),
+                        }
+                        setArticle(article)
+                        setShowArticle(true)
+                    } else {
+                        const article: ArticleType = {
+                            authorAddress: articleById.author,
+                            authorName: "",
+                            authorDesc: "",
+                            authorImg: "",
+                            title: articleById.title,
+                            content: {blocks: []},
+                            coverImg: articleById.image,
+                            readTime: 0,
+                            createdAt: new Date(parseInt(articleById.createDate) * 1000).toDateString(),
+                            id: articleById.id,
+                            likes: 0,
+                            price: parseInt(articleById.price),
+                        }
+                        
+                        setArticle(article)
+                    }
+                }
+            }
+            setLoading(false)
+        }
+
+        if (author && authorId && !isPreview) {
             if (author.name)
                 document.title = `${author.name} - Tales`
-
-            getArticlesByAuthor(author.address)
+            setLoading(true)
+            getArticlesByAuthor(authorId)
         }
-    }, [author, isPreview])
+    }, [author, authorId, isPreview, articleId, user])
 
 
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
 
 
-    if (isAuthorLoading) return <></>
-    if (!author?.address || article === undefined) return <div className='flex items-center justify-center text-lg'>Author not found</div>
+    if (isAuthorLoading || loading) return <Loader />
+    // if (author?.address === undefined || author?.address == "" || article === undefined) return <div className='flex items-center justify-center text-lg'>Author not found</div>
 
+    if (article === undefined) return <div className='flex items-center justify-center text-lg'>Article not found</div> 
 
     return (
         <div className="editor">
@@ -78,20 +223,34 @@ const Article = ({ post, isPreview = false, authorIdForPreview }: PostProps) => 
                         </div>
                     </div>
                     <div className="flex gap-6">
-                        <Button variant="tertiary" size="small" onClick={() => {(post.price > 0) && handleShow()}}>
-                            {getPrice({price: post.price})}
-                        </Button>
-                        <Button variant="primary" size="small" tone="green" onClick={handleShow}>
-                            Subscribe
-                        </Button>
+                        {
+                            !(user?.addr != "" && user?.addr == article.authorAddress) &&
+                            <>
+                                <Button variant="tertiary" size="small" onClick={handlePaidArticle}>
+                                {
+                                    showLogin ? "Login to read" : 
+                                        (article?.price > 0) ? article?.content?.blocks?.length ? "Collected Article" : getPrice({price: article?.price})
+                                    : "Read for free"
+                                }
+                            </Button>
+                            <Button variant="primary" size="small" tone="green" onClick={handleShow}>
+                                Subscribe
+                            </Button>
+                            </>
+                        }
                     </div>
                 </div>
 
                 <div className="max-w-3xl min-w-[50vw] mx-auto py-6">
-                    <Heading as="h1" level="1" color={"green"}>{article.title}</Heading>
                     <div className="mt-8">
-                        <img className="w-full rounded-lg" src={article.coverImg} alt="Cover" />
+                        {
+                            article.coverImg ? 
+                                <img className="rounded-lg mb-6 w-[720px] h-[360px] bg-green-900" src={article.coverImg} />
+                                : <div className="rounded-lg mb-6 w-[720px] h-[360px] bg-green-900"></div>
+                        }
                     </div>
+
+                    <Heading as="h1" level="1" align={"center"} color={"green"}>{article.title}</Heading>
 
                     <div className="mt-8 flex items-center justify-between w-full space-x-4">
                         <div className="flex">
@@ -107,11 +266,14 @@ const Article = ({ post, isPreview = false, authorIdForPreview }: PostProps) => 
                             </Tag>
                         </div>
                         <Tag size="small">
-                            {new Date(article.createdAt).toLocaleDateString()}
+                            {new Date(parseInt(article.createdAt) * 1000).toLocaleDateString()}
                         </Tag>
                     </div>
-                    <div dangerouslySetInnerHTML={{ __html: parser.parse(article.content) }} className="mt-8 font-Satoshi16px text-justify">
-                    </div>
+                    {
+                        showArticle ? <div dangerouslySetInnerHTML={{ __html: parser.parse(article.content) }} className="mt-8 font-Satoshi16px text-justify">
+                        </div>
+                        : <SkeletonLoader />
+                    }
                 </div>
 
                 {/* {
@@ -136,7 +298,7 @@ const Article = ({ post, isPreview = false, authorIdForPreview }: PostProps) => 
                 <div>
                     <div onClick={handleClose} className="absolute top-0 left-0 w-screen h-screen"></div>
                     <div className="absolute top-24 right-20">
-                        <SubscribeModel onSubscribe={handleClose} name={article.authorName} />
+                        <SubscribeModel onSubscribe={handleClose} authorAddress={article.authorAddress} name={article.authorName} />
                     </div>
                 </div>
             }
@@ -150,3 +312,5 @@ function getPrice({price} : {price: number}) {
 }
 
 export default Article;
+
+
